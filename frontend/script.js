ab54e33423147c4915d263fdd88e2738c515a6b3
ElohegyiI <roxnadz78@gmail.com>
2022-01-19 18:08:27 +0100

    Name and food

--- frontend/script.js
+++ frontend/script.js
@@ -1,12 +1,45 @@
-function loadevent() {
+/* const data = ["Gergely Kiss",
+"Krisztián Bui",
+"Buczkó Balázs",
+"András Varga",
+"Borbála Teréz"];
+ */
+
+const data = [
+    {
+        name: "Gergely Kiss",
+        favouriteFood: "Csirke/rizs"
+    },
+    {
+        name: "Krisztián Bui",
+        favouriteFood: "Banán"
+    },
+    {
+        name: "Buczkó Balázs",
+        favouriteFood: "Pizza"
+    },
+    {
+        name: "András Varga",
+        favouriteFood: "Brassói"
+    },
+    {
+        name: "Borbála Teréz",
+        favouriteFood: "Avokádó"
+    },
+
+]
+function loadEvent() {
   console.log("Az oldal betöltődött.");
   
-  const list = document.querySelectorAll("section");
-  
-  for (const elem of list) {
-      elem.insertAdjacentHTML("beforeend", 
-      `<p>text</p>`);
-      
+const root = document.querySelector("#root")
+
+for (const frontendStudent of data) {
+  root.insertAdjacentHTML("beforeend", `
+  <section>
+  <h1>${frontendStudent.name}</h1>
+  <p>${frontendStudent.favouriteFood}</p>
+  </section>`
+  )  
 }
 }
-window.addEventListener("load", loadevent)
+window.addEventListener("load", loadEvent);
